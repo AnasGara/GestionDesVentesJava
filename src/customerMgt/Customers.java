741f1ca533b8b65203890e645ffd083d157cbe55
Anas Gara <anasjr97@gmail.com>
2022-02-28 11:30:04 +0100

    update

--- src/customerMgt/Customers.java
+++ src/customerMgt/Customers.java
@@ -9,11 +9,12 @@ public class Customers {
     List<Customer> customers = new ArrayList<>();
     public void add(Customer c){
         try {
+            //données de Test
             if (customers.contains(c))
                 throw new Exception("Customer already exist!");
             if(c.getPrenom()==null|c.getNom().trim().equals(""))
                 throw new Exception("Missing data:First name");
-
+            customers.add(c);
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
